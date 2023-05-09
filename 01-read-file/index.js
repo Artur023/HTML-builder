e74b74cfae31201c95d00b590c86eff04c8ddece
Artur023 <artur.023@mail.ru>
2023-05-09 16:45:55 +0400

    fix all

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,7 +1,6 @@
 const fs = require("fs");
 const path = require("path");
 const { stdout } = require("process");
-require('colors')
 
 
 const stream = fs.createReadStream(path.join(__dirname, "text.txt"), "utf-8");
@@ -10,5 +9,5 @@ stream.on("data", (chunk) => {
   text += chunk;
 });
 stream.on("end", () => {
-  stdout.write(text.rainbow);
+  stdout.write(text);
 });
